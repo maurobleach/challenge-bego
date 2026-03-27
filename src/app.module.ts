a46c3d64afde42e450165ffa077bb2a56e88a16d
maurobleach <maurobonnet1@gmail.com>
2026-03-27 19:22:39 -0300

    desarrollo de orders y documentacion de uso

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { LocationsModule } from './locations/locations.module';
+import { OrdersModule } from './orders/orders.module';
 import { TrucksModule } from './trucks/trucks.module';
 import { UsersModule } from './users/users.module';
 
@@ -25,6 +26,7 @@ import { UsersModule } from './users/users.module';
     UsersModule,
     TrucksModule,
     LocationsModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
